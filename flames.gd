25f12cb064614af9b927a6f06f127e0ec0870de4
Bradley Carlson <kapitancarlson@gmail.com>
2026-01-19 00:16:38 -0700

    starting the fire

---
+++ flames.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = %AudioStreamPlayer2D
+@onready var sprite_2d: Sprite2D = %Sprite2D
+@onready var collision_shape_2d: CollisionShape2D = %CollisionShape2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	connect("body_entered", _on_body_entered)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func get_picked_up() -> void:
+	sprite_2d.visible = false
+	collision_shape_2d.set_deferred("disabled", true)
+	audio_stream_player_2d.play()
+	await audio_stream_player_2d.finished
+	queue_free()
+
+#for when the fire hits a wall
+func _on_body_entered(body) -> void:
+	sprite_2d.visible = false
+	collision_shape_2d.set_deferred("disabled", true)
+	audio_stream_player_2d.play()
+	await audio_stream_player_2d.finished
+	queue_free()
